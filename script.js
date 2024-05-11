f9250ccd68cef64c6aa9ed628c8fd0c08add78ff
jassi74 <167304101+jassi74@users.noreply.github.com>
2024-05-12 00:30:08 +0530

    contact added

--- script.js
+++ script.js
@@ -22,8 +22,17 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
-   
-    
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
   // toggle menu/navbar script
   $('.menu-btn').click(function(){
