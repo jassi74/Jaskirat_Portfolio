416bcf508926ad46b78e5b3ac960ea8d6b1c0176
jassi74 <167304101+jassi74@users.noreply.github.com>
2024-05-12 19:50:49 +0530

    project section added

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeOut: 1000,
         autoplayHoverPause: true,
         responsive: {
             0:{
@@ -57,7 +57,28 @@ $(document).ready(function(){
             },
             1000:{
                 items: 3,
+                nav: false    
+            }
+        }
+    });
+    $('.carousel2').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
                 nav: false
+            },
+            600:{
+                items: 1,
+                nav: false
+            },
+            1000:{
+                items: 1,
+                nav: false    
             }
         }
     });
